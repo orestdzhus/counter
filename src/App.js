26e3e623a10316bd640e8fcc3fe1e868d4c0106d
orestdzhus <work.orest.dzhus@gmail.com>
2023-11-07 10:23:09 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 
 function App() {
-  return (
-      <div className="App">
-
-      </div>
-  );
+    return (
+        <div className="App">
+            <div>xxx</div>
+        </div>
+    );
 }
 
 export default App;
